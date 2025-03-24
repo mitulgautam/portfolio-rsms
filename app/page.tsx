1b08fe2d0ef1ffe6e333e02617fd1afcc7e6d756
img <mitulgautam@outlook.com>
2025-03-24 23:12:40 +0530

    Font changes added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,15 @@
-import Image from "next/image";
-import "@fontsource/inter"; // Default weight
-import "@fontsource/roboto"; // Alternative
+import {Inter} from "next/font/google"
+
+const inter = Inter({subsets: ['latin'], weight: ['400', '500', '700']})
 
 export default function Home() {
     return (
         <>
-            <head>
-                <link
-                    href="https://fonts.googleapis.com/css2?amily=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap&family=Roboto:wght@400;700&display=swap"
-                    rel="stylesheet"
-                />
-                <title>Mitul Gautam</title>
-            </head>
-            <div className={"sm:mx-24 sm:mt-12 md:mx-48 md:mt-24 flex flex-col"}>
+            <div className={"sm:mx-24 sm:mt-12 md:mx-48 md:mt-24 flex flex-col " + inter.className}>
                 <div className={"flex flex-row"}>
-                    <header className={"md:w-full h-max text-3xl font-bold bg-pink-200"}>
+                    <header className={"md:w-full h-max text-3xl font-bold"}>
                     <span>
-                        <h1 className={"font-inter"}>Hello, I'm Mitul</h1>
+                        <h1 className={"bg-red-200 w-max px-1"}>{"Hello, I'm Mitul"}</h1>
                     </span>
                     </header>
                     <nav className={"w-1/3 flex flex-col"}>
@@ -35,9 +28,9 @@ export default function Home() {
                     <span className={"font-medium text-lg"}>Experienced Senior Software Engineer with 5+ years in finance-based products and enterprise software solutions.<br/>Skilled in designing product architecture (LLD & HLD), developing REST APIs, optimizing SQL queries, and building mobile & web applications.</span>
                 </div>
                 <main className={"flex flex-row"}>
-                    <section className={"px-8 mt-24 font-bold text-2xl"}>Projects Section</section>
-                    <section className={"px-8 mt-24 font-bold text-2xl"}>Thoughts & Ideas Section</section>
-                    <section className={"px-8 mt-24 font-bold text-2xl"}>Bio Section</section>
+                    <section className={"px-8 mt-24 font-medium text-2xl"}>Projects Section</section>
+                    <section className={"px-8 mt-24 font-medium text-2xl"}>Thoughts & Ideas Section</section>
+                    <section className={"px-8 mt-24 font-medium text-2xl"}>Bio Section</section>
                 </main>
                 <footer className={"p-5 text-center"}>Refer for portfolio template <a className={"text-red-700"}
                                                                                       href={"https://rsms.me/"}>www.rsms.me</a>
