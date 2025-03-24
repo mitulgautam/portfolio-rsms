f9406dd14bca9aa064b4666905165c5391ad5988
img <mitulgautam@outlook.com>
2025-03-24 22:36:02 +0530

    Basic page has been setup with name, nav, and section's title.

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,48 @@
 import Image from "next/image";
+import "@fontsource/inter"; // Default weight
+import "@fontsource/roboto"; // Alternative
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    return (
+        <>
+            <head>
+                <link
+                    href="https://fonts.googleapis.com/css2?amily=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap&family=Roboto:wght@400;700&display=swap"
+                    rel="stylesheet"
+                />
+                <title>Mitul Gautam</title>
+            </head>
+            <div className={"sm:mx-24 sm:mt-12 md:mx-48 md:mt-24 flex flex-col"}>
+                <div className={"flex flex-row"}>
+                    <header className={"md:w-full h-max text-3xl font-bold bg-pink-200"}>
+                    <span>
+                        <h1 className={"font-inter"}>Hello, I'm Mitul</h1>
+                    </span>
+                    </header>
+                    <nav className={"w-1/3 flex flex-col"}>
+                        <a href={"#"} className={"px-16 font-bold text-gray-300 text-2xl hover:text-red-700"}>Me</a>
+                        <a href={"#about"}
+                           className={"px-16 font-bold text-gray-300 text-2xl hover:text-red-700"}>About</a>
+                        <a href={"#projects"}
+                           className={"px-16 font-bold text-gray-300 text-2xl hover:text-red-700"}>Project</a>
+                        <a href={"#work"}
+                           className={"px-16 font-bold text-gray-300 text-2xl hover:text-red-700"}>Work</a>
+                        <a href={"#cricket"}
+                           className={"px-16 font-bold text-gray-300 text-2xl hover:text-red-700"}>Cricket</a>
+                    </nav>
+                </div>
+                <div className={"pt-12"}>
+                    <span className={"font-medium text-lg"}>Experienced Senior Software Engineer with 5+ years in finance-based products and enterprise software solutions.<br/>Skilled in designing product architecture (LLD & HLD), developing REST APIs, optimizing SQL queries, and building mobile & web applications.</span>
+                </div>
+                <main className={"flex flex-row"}>
+                    <section className={"px-8 mt-24 font-bold text-2xl"}>Projects Section</section>
+                    <section className={"px-8 mt-24 font-bold text-2xl"}>Thoughts & Ideas Section</section>
+                    <section className={"px-8 mt-24 font-bold text-2xl"}>Bio Section</section>
+                </main>
+                <footer className={"p-5 text-center"}>Refer for portfolio template <a className={"text-red-700"}
+                                                                                      href={"https://rsms.me/"}>www.rsms.me</a>
+                </footer>
+            </div>
+        </>
+    );
 }
