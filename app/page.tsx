c5c104836f63128afde18cb0b7dd05040b90375a
img <mitulgautam@outlook.com>
2025-03-24 23:42:14 +0530

    Mobile view optimized.

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,43 @@
 import {Inter} from "next/font/google"
 
-const inter = Inter({subsets: ['latin'], weight: ['400', '500', '700']})
+const inter = Inter({subsets: ['latin'], weight: ['400', '500', '600', '700']})
 
 export default function Home() {
     return (
         <>
-            <div className={"sm:mx-24 sm:mt-12 md:mx-48 md:mt-24 flex flex-col " + inter.className}>
-                <div className={"flex flex-row"}>
-                    <header className={"md:w-full h-max text-3xl font-bold"}>
+            <div className={"my-24 mx-8 sm:mx-24 sm:mt-12 md:mx-48 md:mt-24 flex flex-col " + inter.className}>
+                <div className={"flex flex-col sm:flex-row"}>
+                    <header className={"md:w-full h-max text-4xl font-bold"}>
                     <span>
-                        <h1 className={"bg-red-200 w-max px-1"}>{"Hello, I'm Mitul"}</h1>
+                        <span className={"bg-red-200 w-max px-1"}>{"Hello, I'm Mitul"}</span>
                     </span>
                     </header>
-                    <nav className={"w-1/3 flex flex-col"}>
-                        <a href={"#"} className={"px-16 font-bold text-gray-300 text-2xl hover:text-red-700"}>Me</a>
+                    <nav className={"sm:w-1/3 flex flex-col mt-24 sm:mt-0"}>
+                        <a href={"#"}
+                           className={"sm:px-16 font-semibold text-gray-300 text-2xl hover:text-red-700"}>Me</a>
                         <a href={"#about"}
-                           className={"px-16 font-bold text-gray-300 text-2xl hover:text-red-700"}>About</a>
+                           className={"sm:px-16 font-semibold text-gray-300 text-2xl hover:text-red-700"}>About</a>
                         <a href={"#projects"}
-                           className={"px-16 font-bold text-gray-300 text-2xl hover:text-red-700"}>Project</a>
+                           className={"sm:px-16 font-semibold text-gray-300 text-2xl hover:text-red-700"}>Project</a>
                         <a href={"#work"}
-                           className={"px-16 font-bold text-gray-300 text-2xl hover:text-red-700"}>Work</a>
+                           className={"sm:px-16 font-semibold text-gray-300 text-2xl hover:text-red-700"}>Work</a>
                         <a href={"#cricket"}
-                           className={"px-16 font-bold text-gray-300 text-2xl hover:text-red-700"}>Cricket</a>
+                           className={"sm:px-16 font-semibold text-gray-300 text-2xl hover:text-red-700"}>Cricket</a>
                     </nav>
                 </div>
-                <div className={"pt-12"}>
-                    <span className={"font-medium text-lg"}>Experienced Senior Software Engineer with 5+ years in finance-based products and enterprise software solutions.<br/>Skilled in designing product architecture (LLD & HLD), developing REST APIs, optimizing SQL queries, and building mobile & web applications.</span>
+                <div className={"pt-4 mt-16 sm:mt-0 font-semibold text-2xl sm:pt-12 sm:hidden"}>
+                    <span>Experienced Senior Software Engineer with 5+ years in finance-based products and enterprise software solutions.</span>
                 </div>
-                <main className={"flex flex-row"}>
-                    <section className={"px-8 mt-24 font-medium text-2xl"}>Projects Section</section>
-                    <section className={"px-8 mt-24 font-medium text-2xl"}>Thoughts & Ideas Section</section>
-                    <section className={"px-8 mt-24 font-medium text-2xl"}>Bio Section</section>
+                <main className={"sm:flex sm:flex-row w-max mt-24 font-bold text-2xl"}>
+                    <section className={"px-8"}>Projects Section</section>
+                    <section className={"px-8"}>Thoughts & Ideas Section</section>
+                    <section className={"px-8"}>
+                        <div className={"w-1/3 hidden sm:block"}>
+                            <p>Experienced Senior Software Engineer with 5+ years in finance-based products and enterprise software solutions.</p>
+                        </div>
+                    </section>
                 </main>
-                <footer className={"p-5 text-center"}>Refer for portfolio template <a className={"text-red-700"}
+                <footer className={"p-4 text-center"}>Refer for portfolio template <a className={"text-red-700"}
                                                                                       href={"https://rsms.me/"}>www.rsms.me</a>
                 </footer>
             </div>
