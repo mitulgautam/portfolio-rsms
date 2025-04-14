7554ea16cbb31d336b73903471d03dd8dfa78f14
img <mitulgautam@outlook.com>
2025-04-14 23:13:55 +0530

    Added some projects data.

--- app/page.tsx
+++ app/page.tsx
@@ -26,14 +26,48 @@ export default function Home() {
                     </nav>
                 </div>
                 <div className={"pt-4 mt-16 sm:mt-0 font-semibold text-2xl sm:pt-12 sm:hidden"}>
-                    <span>Experienced Senior Software Engineer with 5+ years in finance-based products and enterprise software solutions.</span>
+                    <span>Experienced Senior Software Engineer with 5+ years in finance-based products and enterprise software solutions. Skilled in designing product architecture (LLD & HLD), developing REST APIs, optimizing SQL queries, and building mobile & web applications. Proven expertise as a full-stack engineer in both team and solo projects, with experience in leading teams. Passionate about optimizing system performance and enhancing user experience through efficient software solutions.</span>
                 </div>
-                <main className={"sm:flex sm:flex-row w-max mt-24 font-bold text-2xl"}>
-                    <section className={"px-8"}>Projects Section</section>
-                    <section className={"px-8"}>Thoughts & Ideas Section</section>
-                    <section className={"px-8"}>
-                        <div className={"w-1/3 hidden sm:block"}>
-                            <p>Experienced Senior Software Engineer with 5+ years in finance-based products and enterprise software solutions.</p>
+                <main className={"sm:flex sm:flex-row mt-24"}>
+                    <section className={"w-1/2"}>
+                        <div>
+                            <p className={"text-lg font-semibold text-red-900"}>Pay Service</p>
+                            <p className={"text-sm font-normal"}>Designed and implemented scalable database schemas,
+                                developed REST-ful APIs using Java 17 with the Spring Boot framework, and built modern,
+                                responsive front-end interfaces using Next.js 15. Utilized Tailwind CSS and ShadCN UI
+                                for clean, accessible UI design. Experienced in writing optimized SQL queries with
+                                PostgreSQL for efficient data access and management.</p>
+                        </div>
+                        <div className={"pt-4"}>
+                            <p className={"text-lg font-semibold text-red-900"}>Pay Service(Legacy)</p>
+                            <p className={"text-sm font-normal"}>Maintained and supported a legacy system built with
+                                JAX-WS for backend services, OracleDB as the database, and JSP for the frontend. Focused
+                                on optimizing database procedures and improving API performance by refining complex
+                                database views and optimizing SQL joins.</p>
+                        </div>
+                        <div className={"pt-4"}>
+                            <div className={"flex items-center"}><p
+                                className={"text-lg font-semibold text-red-900"}>First Performance</p>
+                                <p className={"text-xs font-medium pl-1 text-red-900"}>(acquired by
+                                    MasterCard)</p></div>
+                            <p className={"text-sm font-normal"}>Contributed to the development of a transaction
+                                cleansing system for credit/debit card data, enhancing the readability of transaction
+                                records. Collaborated in a team of four to design and develop serverless APIs using AWS
+                                Lambda (Python) for efficient data processing. Utilized Pandas to create and update
+                                scripts for large-scale data cleansing, handling datasets with over 5 million records
+                                extracted from user transaction files.</p>
+                        </div>
+                    </section>
+                    <section className={"px-8 w-1/2 font-semibold text-red-900"}>Thoughts & Ideas</section>
+                    <section className={"px-8 w-1/2"}>
+                        <div className={"hidden sm:block"}>
+                            <p className={"text-lg font-semibold text-red-900"}>Summary</p>
+                            <p className={"text-sm font-normal"}>Experienced Senior Software Engineer with 5+ years in
+                                finance-based products and enterprise software solutions. Skilled in designing product
+                                architecture (LLD & HLD), developing REST APIs, optimizing SQL queries, and building
+                                mobile & web applications. Proven expertise as a full-stack engineer in both team and
+                                solo projects, with experience in leading teams. Passionate about optimizing system
+                                performance and enhancing user experience through efficient software solutions.</p>
                         </div>
                     </section>
                 </main>
